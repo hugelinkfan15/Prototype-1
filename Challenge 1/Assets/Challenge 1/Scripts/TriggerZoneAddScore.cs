fc5a2cb152a2975192a6ba333f3d7e9352d79e09
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-09-07 15:58:41 -0500

    Challenge 1 done

---
+++ Challenge 1/Assets/Challenge 1/Scripts/TriggerZoneAddScore.cs
@@ -0,0 +1,22 @@
+﻿/*
+* Kayden Miller
+* CGE 401 Prototype 01
+* Script to add score when player crosses the trigger zone
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneAddScore : MonoBehaviour
+{
+    private bool triggered = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ScoreManager.score++;
+        }
+    }
+}
